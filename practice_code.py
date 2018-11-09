ee5785f2c9f59d394a50d1b645e998b06168d828
Deepak Singh <deepakchandi@yahoo.com>
2018-11-08 22:04:03 -0800

    eda almost done

---
+++ practice_code.py
@@ -0,0 +1,55 @@
+import pandas as pd
+import numpy as np
+
+
+#removing columns
+col_to_remove = (['a1', 'a2', 'a3','a4','a5','h1','h2','h3','h4','h5', 'play_length', 'entered', 'left', 'possession', 'shot_distance', 'original_x', 'original_y', 'converted_x', 'converted_y', 'num', 'away', 'home', 'outof', 'opponent', 'reason', 'elapsed', 'play_id'], axis = 1)
+def remove_col(df, list_of_col):
+    for items in list_of_col:
+        df = df.drop([items], axis = 1)
+    return df
+
+
+
+#removing unnecessary rows
+#rows to remove contain these values:
+
+rows = ['sub', 'timeout', 'uknown', 'unknown', 'Ejection', 'violat', 'traveling', 'team reb', 'foul',
+        'turnover', 'of period', 'lost ball', 'goaltend', 'bad pass', 'illegal', 'jump ball', 'o']
+
+def remove_junk_rows(df, column, list_of_rows):
+    
+    for item in list_of_rows:
+        if item == 'o':
+            df = df[df[column] != 'o']
+        else:
+            df=df[~df[column].str.contains(item)]
+    return df 
+          
+#code for one specific row
+#clutch_time = clutch_time[~clutch_time['type'].str.contains('sub')]              
+
+
+
+#Changes the 'free throw #' with only 'free throw', make rebounds as just rebounds and changes all the shot types to just shot
+f = []
+def change_name(df, column):
+    values = df[column].values
+    for value in values:
+        if "Free" in value:
+            value = 'Free Throw'
+        elif 'rebound' in value:
+            value = 'rebound'
+        else: # "Shot" or "Dunk" or "Layup" or "Roll" or 'Fadea' in value:
+            value = 'Shot'
+        f.append(value)
+        
+        
+        
+#gives you the shooting percent
+def get_percent(df, column1, column2, name):
+    a = df[(df[column1]==name) & ((df[column2]== 'miss')|(df[column2]=='shot'))][[column1, column2]]
+    percent = np.sum(a[column2]=='shot')/a[column2].count()
+    return percent
+
+
